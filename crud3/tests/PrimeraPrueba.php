8d9bf076862069a3b7bdb1d7c455d3df2e842b4d
juancif <cifuenteslozanojuandavid@gmail.com>
2024-08-10 15:39:53 -0500

    se agregan datos a crud3

---
+++ crud3/tests/PrimeraPrueba.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+final class PrimeraPrueba extends TestCase{
+public function testPrimeraPrueba(): void
+{
+$this->assertTrue(true);
+}
+}
+?>
